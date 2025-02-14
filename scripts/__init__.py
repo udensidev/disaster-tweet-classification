90d3bff6ce102cc8d8045a3887fb9661f6270899
udensidev <mmesomaudensi@gmail.com>
2025-02-13 22:00:55 -0600

    Updated project structure

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,2 +1,2 @@
-from .analysis_functions import train_model, make_predictions
-from .data_cleaning import clean_text, vectorize_text
+from .model_utils import train_model, make_predictions
+from .data_utils import clean_text, vectorize_text
